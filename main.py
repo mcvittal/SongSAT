f079b3c95db6b732ca357e8eac7e222792793e43
Alex McVittie <amcvittie@skywatch.co>
2018-10-27 18:04:32 -0400

    Added forest theme

--- main.py
+++ main.py
@@ -11,6 +11,25 @@ import matplotlib.image as mpimg
 import random
 
 
+def arpeggio(x):
+    dic = {0:0, 
+           1:4,
+           2:7,
+           3:12,
+           4:16, 
+           5:12,
+           6:7,
+           7:4,
+           8:0,
+           9:4,
+           10:7,
+           11:12,
+           12:16,
+           13:12,
+           14:7,
+           15:4}
+    return dic[x]
+
 # Generates a thematic song in MIDI from input raster data. Three themes are available with more to come: A calming water song (WATER), a celtic-inspired country grassland style (GRASSLAND), and an intimidating mountain war march (MOUNTAIN). Further fine-tuning can be done within the method. 
 def generate_song(image, theme="WATER", output="/tmp/default.mid"):
     rhythms_lst = {"WATER":[    [1, 1, 1, 0.5, 0.5],
@@ -23,7 +42,9 @@ def generate_song(image, theme="WATER", output="/tmp/default.mid"):
                                 [0.25, 0.25, 0.25, 1.25, -0.5, 0.5, 0.25, 0.25, 0.25, 0.25]],
                    "GRASSLAND":[[1, 0.5, 1, 0.5, 1, 0.5, 1.5],
                                 [1.5, 1.5, 0.5, 0.5, 0.5, 1.5],
-                                [6]]
+                                [6]],
+                   "FOREST":[   [0.5, -0.5, 0.5, -0.5, 0.25, 0.75],
+                                [-0.5, 0.5, -0.5, 0.25, 0.25, 0.25, 0.25, 0.5, -0.5, 0.5]]
                                 }
     accompaniment_lst = {"WATER":[[3, 1],
                                   [1, 1, 1, 1],
@@ -35,16 +56,20 @@ def generate_song(image, theme="WATER", output="/tmp/default.mid"):
                                   [1.5, 0.5, 0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25]],
                      "GRASSLAND":[[3, 3],
                                   [6], 
-                                  [2, 1, 2, 1]]
+                                  [2, 1, 2, 1]],
+                     "FOREST":[   [0.25] * 16, 
+                                  [0.25] * 16]
                                   }
     # Defines how many scale degrees will be used in each theme
     degrees_lst = {"WATER":5, 
                    "MOUNTAIN":6, 
-                   "GRASSLAND":4}
+                   "GRASSLAND":4,
+                   "FOREST":3}
     # Set the tempo of each theme
     tempo={"WATER":130, 
            "MOUNTAIN":100, 
-           "GRASSLAND":130}
+           "GRASSLAND":130,
+           "FOREST":60}
 
     rhythms = rhythms_lst[theme]
     accompaniment = accompaniment_lst[theme]
@@ -66,19 +91,21 @@ def generate_song(image, theme="WATER", output="/tmp/default.mid"):
     pentatonic = list(map(lambda x: (x % degrees_lst[theme]) + 1, pentatonic))
     
     # Shorten the data to be a reasonable song length
-    melody = pentatonic[::int(len(pentatonic) / 65)]
+    melody = pentatonic[::int(len(pentatonic) / 650)]
     
     # Initialize the scale used for the thematic tune
     scales = {"WATER":{1:0, 2:2, 3:4, 4:7, 5:9},
               "MOUNTAIN":{1:0, 2:2, 3:3, 4:4, 5:5, 6:8},
-              "GRASSLAND":{1:0, 2:4, 3:7, 4:9}}
+              "GRASSLAND":{1:0, 2:4, 3:7, 4:9},
+              "FOREST":{1:0, 2:2, 3:4}}
     scale = scales[theme]
 
     x = 0
     # The base octave that this song begins at. Set to an octave above middle C (60) with modifier.
     octaves = {"WATER":60 + 12,
                "MOUNTAIN":60 - 12,
-               "GRASSLAND":60 + 6}
+               "GRASSLAND":60 + 6,
+               "FOREST":60}
     octave = octaves[theme]
     
     # Keep track of when midi events happen 
@@ -132,15 +159,20 @@ def generate_song(image, theme="WATER", output="/tmp/default.mid"):
                     lnote -= 12
                 #print(lnote)
                 if b[i] > 0:
-                    MyMIDI.addNote(track, 1, 
-                               lnote,
-                               time_lower, b[i], 100)
+                    if theme != "FOREST":
+                        MyMIDI.addNote(track, 1, 
+                                       lnote,
+                                       time_lower, b[i], 100)
+                    else:
+                        MyMIDI.addNote(track, 1, 
+                                       lnote + arpeggio(i),
+                                       time_lower, b[i], 100)
                 else:
                     b[i] *= -1
                 time_lower += b[i]
             
         except Exception as e:
-            pass
+            print(e)
     # Add nice chord to end of water theme
     if theme == "WATER":
         MyMIDI.addNote(track, 0, 60, time_upper, 4, 100)
@@ -152,6 +184,7 @@ def generate_song(image, theme="WATER", output="/tmp/default.mid"):
     MyMIDI.writeFile(binfile)
     binfile.close()
 
-generate_song("/home/alex/Documents/spaceapp/landsat_image.tif", theme="GRASSLAND", output="/tmp/grassland.mid")
+generate_song("/home/alex/Documents/spaceapp/landsat_image.tif", theme="FOREST", output="/tmp/WATER.mid")
+
 
    
